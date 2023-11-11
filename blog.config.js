c3dcaa526518a2af78a812b4cd0c574641c3a400
steveBIM <87267288+steveBIM@users.noreply.github.com>
2023-11-11 12:18:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -205,17 +205,17 @@ const BLOG = {
   MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
-    {
-      name: '风を共に舞う気持ち',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
-      cover:
+    //{
+      //name: '风を共に舞う気持ち',
+      //artist: 'Falcom Sound Team jdk',
+      //url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+      //cover:
             'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    },
+    //},
     {
       name: '王都グランセル',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
+      url: 'https://drive.google.com/file/d/1QUle2gbOrW4xlmYeSLufnU_uPWir_H1x/view?usp=sharing',
       cover:
             'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     }
