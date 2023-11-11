f8e0375cf5e1058b18104343d213141290bbbcba
steveBIM <87267288+steveBIM@users.noreply.github.com>
2023-11-11 20:51:24 +0800

    copy1

--- components/DisableCopy.js
+++ components/DisableCopy.js
@@ -6,7 +6,6 @@ import { useEffect } from 'react'
  */
 export default function DisableCopy() {
   useEffect(() => {
-     //(!JSON.parse(siteConfig('CAN_COPY'))) {
       // 全栈添加禁止复制的样式
     document.getElementsByTagName('html')[0].classList.add('forbid-copy')
       // 监听复制事件
@@ -14,7 +13,6 @@ export default function DisableCopy() {
       event.preventDefault() // 阻止默认复制行为
       alert('抱歉，本网页内容不可复制！')
       })
-    //}
   }, [])
 
   return null
