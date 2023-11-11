d3951e00f0bf32a4c4b0dd9ed3e3c43ceb75948b
steveBIM <87267288+steveBIM@users.noreply.github.com>
2023-11-11 16:30:30 +0800

    Test 1

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Steve', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一個普通人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://bimertech.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Blog, Bimer', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Blog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'li.steve2018@gmail.com', // 邮箱地址 例如mail@tangly1024.com
